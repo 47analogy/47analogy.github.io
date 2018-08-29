b09b89ec9ed9918e4dc007e2660a6a7737bc484f
analogy47 <actweed47@gmail.com>
2018-08-29 15:52:33 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+//add plugins and static info
+module.exports = {
+  siteMetadata: {
+    title: 'Tweed Portfolio',
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`, // modifies head tags
+    `gatsby-plugin-catch-links`, // avoids refresh and allow local links (i.e. non-react pages)
+    `gatsby-plugin-sass`, // compiles scss
+    // points to typography.js    
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    // sources data about files from cpu's file system
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+
+
+
+  ], // end plugins
+};
